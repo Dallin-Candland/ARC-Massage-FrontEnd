581ddfca7a2ca498b1a85a25bc75670e0f4647f6
Charlie Fadness <fadness.charlie@outlook.com>
2022-09-02 11:14:55 -0700

    dev branch set up

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Whoo!
+          We are now on the Dev Branch!!!
         </a>
       </header>
     </div>
