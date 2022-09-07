4f1d45054756ce85c8158e446c37eadb5fdd0872
Charlie Fadness <fadness.charlie@outlook.com>
2022-09-06 19:49:09 -0700

    finished with the landing page

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,23 @@
-
+import Header from '../Header/Header';
 import "./main.css";
 
 export default function Main(props) {
   return (
-    <>
-     <h1>Main</h1>
-    </>
+    <div className='mainBackground'>
+      <div className='spaceAboveHeader'> </div>
+      <Header />
+      <section className="main">
+        <div className="opening"></div>
+        <span className="span titleText intro">
+          Professional
+          <br /> Tender Love
+          <br /> and Care.
+        </span>
+        <span className="span titleTextRight">
+          <div className='intro'>Get the rejuvenating feeling your body has been craving.</div>
+          <a className="MTSessionButton">Massage Therapy Sessions</a>
+        </span>
+      </section>
+    </div>
   );
 }
