f697bc4a01a2c38acec77a5f26250e0078823c36
Charlie Fadness <fadness.charlie@outlook.com>
2022-09-06 19:57:43 -0700

    added link to services

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Header from '../Header/Header';
 import "./main.css";
 
@@ -10,12 +11,13 @@ export default function Main(props) {
         <div className="opening"></div>
         <span className="span titleText intro">
           Professional
+          {/* TAKE OUT br */}
           <br /> Tender Love
           <br /> and Care.
         </span>
         <span className="span titleTextRight">
           <div className='intro'>Get the rejuvenating feeling your body has been craving.</div>
-          <a className="MTSessionButton">Massage Therapy Sessions</a>
+          <Link to="services" className="MTSessionButton">Massage Therapy Sessions</Link>
         </span>
       </section>
     </div>
