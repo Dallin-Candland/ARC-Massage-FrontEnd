1914da70429585e0a3ff10c2b1e9e989a1e75991
Charlie Fadness <fadness.charlie@outlook.com>
2022-09-15 14:51:05 -0700

    removed unsuded variabled for deployment

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -8,32 +8,10 @@ import {
 import { db } from "./db/db";
 import "./services.css";
 import Intakeq from "../intakeq/intakeq";
-import { Accordion, Tab, Tabs } from "react-bootstrap";
+import { Tab, Tabs } from "react-bootstrap";
 
 export default function Services(props) {
-  const sessionInfo = db.map((el) => {
-    return (
-      <Grid item className="sessionWidthGrid">
-        <img className="cardImage" src={el.image} alt="data" />
-        <Card className="card" sx={{ maxWidth: 345 }}>
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              {el.title}
-            </Typography>
-            <Typography>{el.addOn && el.addOn}</Typography>
-            <Typography variant="body2" color="text.secondary">
-              {el.bio}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <div className="priceButton">
-              {el.addOn && "+"}${el.cost}
-            </div>
-          </CardActions>
-        </Card>
-      </Grid>
-    );
-  });
+
 
   const basicMassage = (service) => {
     let result = db.filter((filter) => filter.type === service);
