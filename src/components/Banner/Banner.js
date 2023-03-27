1c792ed8c5f8ec14c80b632fe23aa6c919ad192f
Charlie Fadness <fadness.charlie@outlook.com>
2023-03-26 20:34:11 -0700

    fixed banner for netlify

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,11 +1,8 @@
 import EstheticSale from './esthetic/esthetic';
 
 let setEndDate = new Date('April 26, 23 01:00:40 GMT+00:00');
-let temp = new Date('March 27, 23 01:00:40 GMT+00:00');
+let setStartDate = new Date('March 27, 23 01:00:40 GMT+00:00');
 
-
-
-let setStartDate = new Date('March 22, 23 01:00:40 GMT+00:00');
 
 let startDate = setStartDate.getTime();
 let endDate = setEndDate.getTime();
