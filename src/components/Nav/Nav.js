e6b5d08d59d192a0c0e6ff3e5ebd6f184c560b17
Charlie Fadness <fadness.charlie@outlook.com>
2023-05-03 21:41:29 -0700

    removed unused variable

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Intakeq from '../intakeq/intakeq';
-import Dropdown from 'react-bootstrap/Dropdown';
 
 export default function Nav(props) {
   const [open, setOpen] = useState(false);
